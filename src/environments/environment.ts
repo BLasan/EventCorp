5ff20026376c24947c12199846e177911efeceb9
Sankha Rathnayake <sankharathnayaka@gmail.com>
2019-08-19 12:08:03 +0530

    firebase configuration details added to environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA95SG6_4tkcDHDySiuQfVt9cbm_kyUwhk",
+    authDomain: "eventcorppro.firebaseapp.com",
+    databaseURL: "https://eventcorppro.firebaseio.com",
+    projectId: "eventcorppro",
+    storageBucket: "eventcorppro.appspot.com",
+    messagingSenderId: "886719532814",
+    appId: "1:886719532814:web:9424058ace3d13af"
+  }
 };
