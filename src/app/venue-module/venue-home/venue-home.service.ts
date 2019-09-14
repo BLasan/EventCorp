57258f5961deeb7061feeb9bc5300aacf33597e3
Sankha Rathnayake <sankharathnayaka@gmail.com>
2019-09-14 17:46:32 +0530

    venue-home service files created and created a filter table with venues

---
+++ src/app/venue-module/venue-home/venue-home.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VenueHomeService {
+
+  private venuesCollection: AngularFirestoreCollection<any>;
+  venues: Observable<any[]>;
+  constructor(private readonly afs: AngularFirestore) {
+
+    this.venuesCollection = afs.collection<any>('Venues');
+    this.venues = this.venuesCollection.snapshotChanges().pipe(map(
+      actions => actions.map(a => {
+        const data = a.payload.doc.data() as any;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      })
+    ));
+
+   }
+
+   myForm = new FormGroup({
+    Name: new FormControl(''),
+    ac: new FormControl(''),
+    car_parking: new FormControl(''),
+    fee: new FormControl(''),
+    seating_capacity: new FormControl('')
+  });
+
+
+  getOrders() {
+    return this.venues;
+  }
+}
