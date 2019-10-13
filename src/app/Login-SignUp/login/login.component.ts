60fb03981fd4543a40ecb0c461e2405ea51ae73d
BLasan <benuraab@gmail.com>
2019-10-13 15:27:13 +0530

    errors checking(1)

--- src/app/Login-SignUp/login/login.component.ts
+++ src/app/Login-SignUp/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'app/services/login.services';
-
+import {redirect_to} from 'scripts/redirect_to';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -17,7 +17,7 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
     
     if(localStorage.getItem('remember_me')=='true'){
-     //(<HTMLInputElement>document.getElementById('user_name')).value=localStorage.getItem('user_email');
+     (<HTMLInputElement>document.getElementById('user_name')).value=localStorage.getItem('user_email');
     }
   }
 
@@ -36,8 +36,7 @@ export class LoginComponent implements OnInit {
           this.login_service.activateRememberUser(email);
         }
         else this.login_service.destroyRememberUser();
-        const redirect_to=require('../../../scripts/redirect_to.js');
-        redirect_to.redirect_to(this.validation.role);
+        redirect_to(this.validation.role);
       }
      
       else
