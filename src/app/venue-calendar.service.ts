abe74f76fd282e6fe5e35dbe9f9ce98be6e806f2
Sankha Rathnayake <sankharathnayaka@gmail.com>
2019-11-11 13:09:18 +0530

    Now Venue owners will be able to see only the events which are relevant to them

--- src/app/venue-calendar.service.ts
+++ src/app/venue-calendar.service.ts
@@ -3,16 +3,20 @@ import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { tap,map } from 'rxjs/operators';
+import { LoginService } from './services/login.services';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VenueCalendarService {
 
-  constructor(private db:AngularFirestore) { }
+  constructor(
+    private db:AngularFirestore,
+    private loginService: LoginService
+    ) { }
   getData():Observable<any[]>{
     
-    return this.db.collection('events').valueChanges().pipe(
+    return this.db.collection('register_user').doc(this.loginService.currentUser()).collection('events').valueChanges().pipe(
       tap(events=> console.log(events)), //this is added to observe the data which are retrieving from the database and passed to the 'events' array
       map(events => events.map(event => { //the data retrived from the database are retrieved as timestamp. So here it's getting map to a date format 
         let data:any=event;
