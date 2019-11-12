383141d1e320ad8f10601e24490768953cba9d74
Sankha Rathnayake <sankharathnayaka@gmail.com>
2019-11-12 19:42:22 +0530

    Old data submitting method in venue registration form removed and formatted the code

--- src/app/venue-module/venue-home/venue-add/venue-add.component.ts
+++ src/app/venue-module/venue-home/venue-add/venue-add.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { tap, first } from 'rxjs/operators';
-import { LoginService } from 'app/services/login.services';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule
+} from "@angular/forms";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { tap, first } from "rxjs/operators";
+import { LoginService } from "app/services/login.services";
 
 @Component({
-  selector: 'app-venue-add',
-  templateUrl: './venue-add.component.html',
-  styleUrls: ['./venue-add.component.scss']
+  selector: "app-venue-add",
+  templateUrl: "./venue-add.component.html",
+  styleUrls: ["./venue-add.component.scss"]
 })
 export class VenueAddComponent implements OnInit {
-
-  myForm1 : FormGroup;
+  myForm1: FormGroup;
 
   loading = false;
   success = false;
@@ -21,25 +26,22 @@ export class VenueAddComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private afs: AngularFirestore,
-    private loginService: LoginService) { }
+    private loginService: LoginService
+  ) {}
 
   ngOnInit() {
-
     this.myForm1 = this.fb.group({
-      
-      v_name: ['',Validators.required],
-      venue_address: ['',Validators.required],
-      tel_no: ['',Validators.required],
-      ac: ['',Validators.required],
-      car_parking: ['',Validators.required],
-      fee: ['',Validators.required],
-      seating_capacity: ['',Validators.required],
-      // nameToLowerCase: ['',Validators.required]      
+      v_name: ["", Validators.required],
+      venue_address: ["", Validators.required],
+      tel_no: ["", Validators.required],
+      ac: ["", Validators.required],
+      car_parking: ["", Validators.required],
+      fee: ["", Validators.required],
+      seating_capacity: ["", Validators.required]
+      // nameToLowerCase: ['',Validators.required]
+    });
 
-    })
-
-    this.preloadData();     //storing data in the database is working even WITHOUT this function
-
+    this.preloadData(); //storing data in the database is working even WITHOUT this function
   }
 
   async submitHandler1() {
@@ -48,20 +50,20 @@ export class VenueAddComponent implements OnInit {
     const formValue = this.myForm1.value;
 
     try {
-      await this.afs.collection('register_user').doc(this.loginService.currentUser()).collection('venue').doc('hall').set(formValue);
+      this.createDocumentAtUser(formValue);
+      this.createDocumentAtVenues(formValue);
       this.success = true;
-    } catch(err) {
-      console.error(err)
+      // await this.afs.collection('register_user').doc(this.loginService.currentUser()).collection('venue').doc('hall').set(formValue);
+      // this.success = true;
+    } catch (err) {
+      console.error(err);
     }
-
-    this.createDocumentAtUser(formValue);
-    this.createDocumentAtVenues(formValue);
 
     this.loading = false;
   }
 
-  createDocumentAtVenues(value){
-    return this.afs.collection('Venues').add({
+  createDocumentAtVenues(value) {
+    return this.afs.collection("Venues").add({
       v_name: value.v_name,
       nameToSearch: value.v_name.toLowerCase(),
       venue_address: value.venue_address,
@@ -69,32 +71,40 @@ export class VenueAddComponent implements OnInit {
       ac: value.ac,
       car_parking: value.car_parking,
       fee: parseInt(value.fee, 10),
-      seating_capacity: parseInt(value.seating_capacity, 10),     
+      seating_capacity: parseInt(value.seating_capacity, 10)
       // avatar: avatar
     });
   }
 
-  createDocumentAtUser(value){
-    return this.afs.collection('register_user').doc(this.loginService.currentUser()).collection('venue').doc('hall').set({
-      v_name: value.v_name,
-      nameToSearch: value.v_name.toLowerCase(),
-      venue_address: value.venue_address,
-      tel_no: parseInt(value.tel_no, 10),
-      ac: value.ac,
-      car_parking: value.car_parking,
-      fee: parseInt(value.fee, 10),
-      seating_capacity: parseInt(value.seating_capacity, 10),     
-      // avatar: avatar
-    });
+  createDocumentAtUser(value) {
+    return this.afs
+      .collection("register_user")
+      .doc(this.loginService.currentUser())
+      .collection("venue")
+      .doc("hall")
+      .set({
+        v_name: value.v_name,
+        nameToSearch: value.v_name.toLowerCase(),
+        venue_address: value.venue_address,
+        tel_no: parseInt(value.tel_no, 10),
+        ac: value.ac,
+        car_parking: value.car_parking,
+        fee: parseInt(value.fee, 10),
+        seating_capacity: parseInt(value.seating_capacity, 10)
+        // avatar: avatar
+      });
   }
 
-  preloadData() {           //storing data in the database is working even WITHOUT this function
-    this.afs.doc('Venues/Kjn0JWBKdOnUlBwuE93S').valueChanges().pipe(
-      tap(data => {
-        this.myForm1.patchValue(data)
-      })
-    )
-    .subscribe()
+  preloadData() {
+    //storing data in the database is working even WITHOUT this function
+    this.afs
+      .doc("Venues/Kjn0JWBKdOnUlBwuE93S")
+      .valueChanges()
+      .pipe(
+        tap(data => {
+          this.myForm1.patchValue(data);
+        })
+      )
+      .subscribe();
   }
-
 }
