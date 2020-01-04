f58be3815a8e6cbfc53034c03d3081cd195a71db
Sankha Rathnayake <2017cs143@stu.ucsc.cmb.ac.lk>
2020-01-04 12:31:00 +0530

    query-reply-dialog configured

--- src/app/Modules/Moderator-Module/moderator-queries/moderator-queries.component.ts
+++ src/app/Modules/Moderator-Module/moderator-queries/moderator-queries.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ModeratorService } from "../../../services/moderator.service";
 import { AngularFirestore } from "@angular/fire/firestore";
 import { Router, Params } from '@angular/router';
+import {MatDialog, MatDialogConfig} from "@angular/material";
+import { QueryReplyDialogComponent } from '../query-reply-dialog/query-reply-dialog.component';
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: 'app-moderator-queries',
@@ -16,12 +19,34 @@ export class ModeratorQueriesComponent implements OnInit {
   constructor(
     private moderatorService: ModeratorService,
     private db: AngularFirestore,
-    private router: Router
+    private router: Router,
+    private dialog: MatDialog
   ) { }
 
   ngOnInit() {
     this.getData();
   }
+
+  openDialog() {
+
+    const dialogConfig = new MatDialogConfig();
+
+    // dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+
+    dialogConfig.data = {                          //data which is going to passed into the QueryReplyDialogComponenet
+      id: 1,
+      title: 'Angular For Beginners'
+  };
+
+    this.dialog.open(QueryReplyDialogComponent, dialogConfig);
+    
+    const dialogRef = this.dialog.open(QueryReplyDialogComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(
+        data => console.log("\nDialog output:", data)
+    );    
+}
 
   getData(){
     this.moderatorService.getQueries()
--- src/app/Modules/Moderator-Module/query-reply-dialog/query-reply-dialog.component.ts
+++ src/app/Modules/Moderator-Module/query-reply-dialog/query-reply-dialog.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatButtonModule } from "@angular/material/button";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
+import { FormGroup, FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-query-reply-dialog',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QueryReplyDialogComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  description: string;
+
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<QueryReplyDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) data             //retriving the data which was passed into this component from moderator-queries componenet
+  ) {
+    this.description = data.description;
+  }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      description: [this.description, []],
+      // ...
+    });
+  }
+
+  save() {
+    this.dialogRef.close(this.form.value);
+    // this.close();
+  }
+
+  close() {
+    this.dialogRef.close();
   }
 
 }
